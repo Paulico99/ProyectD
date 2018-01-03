61431cced6acae920a863da47d2118637ebc59d9
cesar-soto96@hotmail.com <cesar-soto96@hotmail.com>
2018-01-03 01:26:26 -0700

    Se realizo el sitio principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,64 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { Platform,MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { ListPage } from '../pages/list/list';
+import { HomePage } from '../pages/home/home';
+
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  //rootPage:any = TabsPage;
+  @ViewChild(Nav) nav: Nav;
+  rootPage = HomePage && TabsPage;
+  
+  
+  pages: Array<{title: string}>;
+  
+  constructor(
+    public platform: Platform,
+    public menu: MenuController,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
+      this.inicializarApp();
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+      this.pages = [
+        {title: 'Bienvenido al Proyecto'},
+        {title: 'Perfil'},
+        {title: 'Ayuda'},
+        {title: 'Informacion'},
+        {title: 'Cerrar Sesion'},      
+      ]
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+   
+  }
+  inicializarApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(pages) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(pages.component);
+
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  pet: string = "Populares";
+  isAndroid: boolean = false;
+  constructor(public navCtrl: NavController,platform: Platform) {
+    this.isAndroid = platform.is('android');
   }
-
+  slides = [
+    {
+      title: "Bienvenido a ...!",
+      description: "paja paja.",
+      image: "assets/imgs/camino.jpg",
+    },
+    {
+      title: "Que significa esto?",
+      description: "<b>paja paja</b> La paja es lo mejor.",
+      image: "assets/imgs/parque.jpg",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "assets/img/ica-slidebox-img-3.png",
+    }
+  ];
 }
